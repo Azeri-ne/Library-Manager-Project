ccf95ac67f262c6b7f29f1cec0814e8b3b1153b2
Azeri-ne <alterna811@gmail.com>
2024-08-09 21:43:31 +0800

    Added "User" class

--- main.cpp
+++ main.cpp
@@ -19,12 +19,37 @@ class Book
 			this -> available = true;
 		}
 		
+		// Test
+		void testPrintInfo()
+		{
+			std::cout << title << ' ' << author << ' ' << isbn << '\n';
+		}
+		
 	// idk why we need getters and setters, the Library class is gonna provide the new books anyway?
+};
+
+class User
+{
+	private:
+		std::string userId;
+		std::string name;
+		// std::string[] borrowedBooks; (I dunno how to make a list of strings)
+		
+	public:
+		User(std::string userId, std::string name)
+		{
+			this -> userId = userId;
+			this -> name = name;
+		}
 };
 
 int main()
 {
 	std::cout << "yo" << '\n';
+	
+	// Test
+	Book testBook1("Bible", "Jesus", "3434");
+	testBook1.testPrintInfo();
 	
 	return 0;
 }
