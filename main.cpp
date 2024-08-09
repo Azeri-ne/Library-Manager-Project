97a05fdc4bf9eaf058beaa67f8547c8e9e6252ee
Azeri-ne <alterna811@gmail.com>
2024-08-09 21:27:47 +0800

    Commented stuff

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ class Book
 			this -> isbn = isbn;
 			this -> available = true;
 		}
+		
+	// idk why we need getters and setters, the Library class is gonna provide the new books anyway?
 };
 
 int main()
